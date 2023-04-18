2ee356e5e6c8978fde54ac4eb7357b5553a10160
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-04-18 23:11:18 +0530

    update search Page

--- src/Components/navbar/Header/Headersearch.jsx
+++ src/Components/navbar/Header/Headersearch.jsx
@@ -4,12 +4,18 @@ import {
   LocationCity,
   Minimize,
   Person,
+  Add,
+  Remove,
+  CalendarViewDay,
 } from "@mui/icons-material";
 import {
   Box,
   Button,
+  FormControl,
   Grid,
   IconButton,
+  OutlinedInput,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
@@ -23,9 +29,7 @@ import { DateRangePicker, DateRange } from "react-date-range";
 import { format } from "date-fns";
 import { useRouter } from "next/router";
 
-const Headersearch = () => {
-  const [value, setValue] = React.useState([null, null]);
-
+export const SearchHotels = (second) => {
   const [openDate, setOpenDate] = useState(false);
   const [openOptions, setOpenOptions] = useState(false);
   const [options, setOptions] = useState({ adult: 1, children: 0, room: 1 });
@@ -60,27 +64,42 @@ const Headersearch = () => {
       option: options,
     };
     console.log(obj);
-    navigation.push("/hotels");
+    navigation.push("/search/searchelements");
   };
   return (
-    <Fragment>
-      <Box
-        border="3px solid #febb02"
-        className="Headersearch flex items-center bg-white rounded-sm w-full"
-      >
-        <div className="headerSearchItem flex-3/4 text-cyan-950">
-          <BedOutlined></BedOutlined>{" "}
-          <input
-            onChange={(e) => setLocation(e.target["value"])}
-            placeholder="Where are you going?"
-            className="headerSearchBar"
+    <Box
+      border="3px solid #febb02"
+      className="Headersearch  flex items-center bg-white rounded-sm w-full gap-2 justify-between backdrop-grayscale-0 text-cyan-950"
+    >
+      <Stack direction={"row"} flex={1.5}>
+        <FormControl sx={{ width: "100%", p: 0.5 }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-search"
+            size="small"
+            className="w-full"
+            placeholder="Where are you going?..."
+            color="primary"
+            sx={{
+              p: 0,
+              m: 0,
+              border: "none",
+              height: "44px",
+              overflow: "hidden",
+            }}
+            startAdornment={
+              <IconButton
+                sx={{
+                  height: "100%",
+                }}
+              >
+                <BedOutlined />
+              </IconButton>
+            }
           />
-        </div>
-
-        <Box
-          className="headerSearchItem text-blue-950 flex-1 relative"
-          sx={{ "&>.newclass:": { width: "100%" } }}
-        >
+        </FormControl>
+      </Stack>
+      <Stack direction={"row"} flex={1} position={"relative"}>
+        <Stack direction={"row"} gap={1}>
           <CalendarTodayOutlined></CalendarTodayOutlined>
           <span
             onClick={() => setOpenDate(!openDate)}
@@ -89,111 +108,107 @@ const Headersearch = () => {
             date[0].endDate,
             "eee d MMM"
           )}`}</span>
-          {openDate && (
-            <DateRange
-              className="absolute  top-10 right-0 left-0"
-              onRangeFocusChange={() => console.log(date)}
-              editableDateInputs={true}
-              // @ts-ignore
+        </Stack>
+        {openDate && (
+          <DateRange
+            className="absolute  top-8 right-0 left-0"
+            onRangeFocusChange={() => console.log(date)}
+            editableDateInputs={true}
+            // @ts-ignore
 
-              onChange={(item) => handleDateChange(item)}
-              moveRangeOnFirstSelection={false}
-              ranges={date}
-            />
-          )}
-        </Box>
-        <div className="headerSearchItem flex-1 relative">
-          <Person></Person>{" "}
+            onChange={(item) => handleDateChange(item)}
+            moveRangeOnFirstSelection={false}
+            ranges={date}
+          />
+        )}
+      </Stack>
+      <Stack direction={"row"} position={"relative"} flex={1}>
+        <Stack direction={"row"} gap={1}>
+          <Person></Person>
           <span
             onClick={() => setOpenOptions(!openOptions)}
             className="headerSearchtext"
           >{`adult ${options.adult} . children ${options.children} . room ${options.room}`}</span>
-          {openOptions && (
-            <div className="option absolute top-10 right-0 left-0 w-24 bg-white border text-cyan-950">
-              <div className="optionItem">
-                <span className="label">Adult</span>
-                <div className="counter">
-                  <button
-                    className="minus"
-                    disabled={options.adult <= 1}
-                    onClick={() => handleOption("adult", "d")}
-                  >
-                    -
-                  </button>
-                  <span className="value">{options.adult}</span>
-                  <button
-                    className="plus"
-                    onClick={() => handleOption("adult", "i")}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <div className="optionItem">
-                <span className="label">Children</span>
-                <div className="counter">
-                  <button
-                    className="minus"
-                    disabled={options.children <= 0}
-                    onClick={() => handleOption("children", "d")}
-                  >
-                    -
-                  </button>
-                  <span className="value">{options.children}</span>
-                  <button
-                    className="plus"
-                    onClick={() => handleOption("children", "i")}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <div className="optionItem">
-                <span className="label">Room</span>
-                <div className="counter">
-                  <button
-                    className="minus"
-                    disabled={options.room <= 1}
-                    onClick={() => handleOption("room", "d")}
-                  >
-                    -
-                  </button>
-                  <span className="value">{options.room}</span>
-                  <button
-                    className="plus"
-                    onClick={() => handleOption("room", "i")}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <div className="optionItem done-btn">
-                <Button
-                  className="done"
-                  variant="outlined"
-                  color="primary"
-                  sx={{ bgcolor: "transparent" }}
-                  onClick={() => setOpenOptions(!openOptions)}
+        </Stack>
+
+        {openOptions && (
+          <div className="flex p-8 flex-col gap-2 rounded absolute top-8 right-0 left-0 w-full bg-white border text-cyan-950">
+            <div className="optionItem flex justify-between items-center ">
+              <span className="label">Adult</span>
+              <div className="counter border p-1">
+                <IconButton
+                  className="minus "
+                  disabled={options.adult <= 1}
+                  onClick={() => handleOption("adult", "d")}
                 >
-                  Done
-                </Button>
+                  <Remove />
+                </IconButton>
+                <span className="value">{options.adult}</span>
+                <IconButton
+                  className="plus"
+                  onClick={() => handleOption("adult", "i")}
+                >
+                  <Add />
+                </IconButton>
               </div>
             </div>
-          )}
-        </div>
-        <div className="headerSearchItem flex-1">
-          <Button
-            sx={{ bgcolor: "#003B95" }}
-            variant="contained"
-            onClick={handleSearch}
-            className="headerSearchtext"
-          >
-            Search
-          </Button>
-        </div>
-      </Box>
-    </Fragment>
+            <div className="optionItem optionItem flex justify-between items-center p-2">
+              <span className="label">Children</span>
+              <div className="counter">
+                <IconButton
+                  className="minus"
+                  disabled={options.children <= 0}
+                  onClick={() => handleOption("children", "d")}
+                >
+                  <Remove />
+                </IconButton>
+                <span className="value">{options.children}</span>
+                <IconButton
+                  className="plus"
+                  onClick={() => handleOption("children", "i")}
+                >
+                  <Add />
+                </IconButton>
+              </div>
+            </div>
+            <div className="optionItem optionItem flex justify-between items-center p-2">
+              <span className="label">Room</span>
+              <div className="counter">
+                <IconButton
+                  className="minus"
+                  disabled={options.room <= 1}
+                  onClick={() => handleOption("room", "d")}
+                >
+                  <Remove />
+                </IconButton>
+                <span className="value">{options.room}</span>
+                <IconButton
+                  className="plus"
+                  onClick={() => handleOption("room", "i")}
+                >
+                  <Add />
+                </IconButton>
+              </div>
+            </div>
+            <div className="optionItem optionItem flex justify-center items-center p-2">
+              <Button
+                className="done w-full"
+                variant="outlined"
+                color="primary"
+                sx={{ bgcolor: "transparent" }}
+                onClick={() => setOpenOptions(!openOptions)}
+              >
+                Done
+              </Button>
+            </div>
+          </div>
+        )}
+      </Stack>
+      <Stack direction={"row"}>
+        <Button onClick={handleSearch} size="large" variant="outlined">
+          Search
+        </Button>
+      </Stack>
+    </Box>
   );
 };
-
-export default Headersearch;
