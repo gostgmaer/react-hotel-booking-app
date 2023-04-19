80aae3b4caf24db3563b0e92797132cee04739ac
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-04-19 22:58:41 +0530

    asdad

--- src/Pages/search/[searchhotel]/index.js
+++ src/Pages/search/[searchhotel]/index.js
@@ -1,7 +1,30 @@
+import Hotelsfeatures from "@/Components/HotelsFeatures/Hotelsfeatures"
+import HotelSearchSidebar from "@/Components/List/hotelSearchSidebar"
+import Layout from "@/layout"
+import { Box } from "@mui/material"
+import { hotelList } from "../../../../public/assets/Mock/bookingAll"
 
 const index = () => {
   return (
-    <div>index</div>
+   <Layout>
+    <Box
+      display={"flex"}
+      justifyContent="space-around"
+      alignItems={"flex-start"}
+      bgcolor='f2f2f2'
+      padding='0 50px'
+      gap={1}
+      className="hotelList">
+      <Box  bgcolor={"#FEBB02"} className="leftSide" flex={1}>
+        <HotelSearchSidebar />
+      </Box>
+      <Box className="rightSide"  >
+        {hotelList.result.map((hotel) => (
+          <Hotelsfeatures key={hotel.id}></Hotelsfeatures>
+        ))}
+      </Box>
+    </Box>
+   </Layout>
   )
 }
 
